05ba154a77516d28af11f350575703f494539e44
Horace Grindelwald <rossbstrachan@gmail.com>
2018-02-23 14:22:19 +0000

    Update teams.py
    Added option to pick team names

--- teams.py
+++ teams.py
@@ -10,9 +10,16 @@ total = input("How many players are there in total? ")
 if int(total) % 2 == 1:
   print(colored("\n[CAUTION]! The number of players you have chosen is odd. That forces one team to have more players than the other.", 'red'))
   print()
+
+team_gen = input("Would you like to specify team names? ").lower()
+if team_gen == "yes" or team_gen == "y":
+  alt_teamA = input("\nWhat would you like to call Team A? ").title()
+  alt_teamB = input("What would you like to call Team B? ").title()
 else:
-  print()
+  alt_teamA = "Team A"
+  alt_teamB = "Team B"
   
+print()
 while x <= int(total):
   invite = input("Player #" + str(x) + ": ").title()
   players.append(str(invite))
@@ -29,9 +36,9 @@ while len(players) > 0:
     players.remove(playerB)
 
 print()
-print("Team A: ", end='')
+print(alt_teamA + ": ", end='')
 for i in range(0, len(teamA)):
-  print(teamA[i], end='\t')
-print("\n\nTeam B: ", end='')
+  print(teamA[i], end=' || ')
+print("\n\n" + alt_teamB + ": ", end='')
 for i in range(0, len(teamB)):
-  print(teamB[i], end='\t')
+  print(teamB[i], end=' || ')
